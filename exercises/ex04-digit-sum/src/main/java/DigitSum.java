f26a042de27b0973b3c8de438a814573928f8d0f
bellaP222 <bellalagoat@gmail.com>
2026-09-16 21:36:23 -0400

    exercise 4 done!

--- exercises/ex04-digit-sum/src/main/java/DigitSum.java
+++ exercises/ex04-digit-sum/src/main/java/DigitSum.java
@@ -21,7 +21,13 @@ public class DigitSum {
      * @return the sum of its decimal digits
      */
     public static int digitSum(int n) {
-        // TODO: complete
-        return 0;
+        int num = Math.abs(n);
+        int sum = 0;
+        while (num > 0) {
+            int digit = num % 10;  // % symbol gives you whole number remainder
+            sum += digit;
+            num = num / 10;
+        }
+        return sum;
     }
 }
